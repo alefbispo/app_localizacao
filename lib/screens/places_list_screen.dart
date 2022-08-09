913b0a9bb7ab144ac5f2ab71080267289a6115d9
alefbispo <Blacksabbath21@>
2022-08-08 22:24:55 -0300

    criando as primeiras paginas e aplicando rotas nomeadas

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,26 @@
+import 'package:app_localizacao/utils/app_routes.dart';
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meus Lugares'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
